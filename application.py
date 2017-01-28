2b4c685364dcd1c61038711c13205cbab7cc267d
samuel-peers <samuelpeers@gmail.com>
2017-01-28 10:51:51 -0600

    Added Flask-PyMongo to requirements

--- application.py
+++ application.py
@@ -1,17 +1,21 @@
+"""
+application.py
+"""
+
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+mongo = PyMongo(application)
 application.config.from_object('config')
 
 @application.route("/", methods=['GET', 'POST'])
 def home_page():
     """ The homepage """
-    return "hi y'all!!"
+    users = mongo.db.users.find()
+    return "".join([result["name"] for result in users])
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
     application.run()
