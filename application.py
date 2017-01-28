1da730dcb8449c5b6da62e07dd151ae8640d9165
samuel-peers <samuelpeers@gmail.com>
2017-01-28 10:56:05 -0600

    Testing out Flask-PyMongo

--- application.py
+++ application.py
@@ -3,16 +3,18 @@ application.py
 """
 
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+mongo = PyMongo(application)
 application.config.from_object('config')
 
 @application.route("/", methods=['GET', 'POST'])
 def home_page():
     """ The homepage """
-    return "hi y'all!! :)"
-
+    users = mongo.db.users.find()
+    return "".join([result["name"] for result in users])
 
 # run the app.
 if __name__ == "__main__":
