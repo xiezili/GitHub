b66f2b620954a150b696b46aa55b429a4ae980ba
samuel-peers <samuelpeers@gmail.com>
2017-01-30 13:38:31 -0600

    Created Services module

---
+++ Application/Services.py
@@ -0,0 +1,23 @@
+"""
+Services.py
+"""
+import sys
+from Persistence.DataAccessObject import DataAccessObject
+
+class Services(object):
+    """Services"""
+    dataAccessService = None
+
+    @staticmethod
+    def create_data_access(dbName):
+        """create_data_access"""
+        Services.dataAccessService = DataAccessObject(dbName)
+
+    @staticmethod
+    def get_data_access():
+        """get_data_access"""
+        if Services.dataAccessService is None:
+            print "Connection to data access has not been established"
+            sys.exit(0)
+
+        return Services.dataAccessService
