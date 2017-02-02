c070fbb84a6f41db8be4cdb06bc1959dea315ad7
samuel-peers <samuelpeers@gmail.com>
2017-02-02 10:47:02 -0600

    Removed feature that is not done-done

--- Persistence/DataAccessObject.py
+++ Persistence/DataAccessObject.py
@@ -9,14 +9,6 @@ class DataAccessObject(DataAccessInterface):
     def __init__(self, name):
         self.mongo = MongoClient()[name]
 
-    def insert_user(self, userName):
-        """Insert a user into the DB"""
-        pass
-
-    def get_user(self, userName):
-        """Grab a user from the DB"""
-        pass
-
     def get_all_users(self):
         """Return a list of all the users"""
         return "".join([result["name"] for result in self.mongo.users.find({})])
