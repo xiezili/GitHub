ff4a003f892e35a9171a7fe277b9f49ff638f689
samuel-peers <samuelpeers@gmail.com>
2017-02-04 20:28:27 -0600

    Implemented 3 random question feature

--- application.py
+++ application.py
@@ -1,29 +1,51 @@
 """
 application.py
 """
-from flask import Flask, render_template, flash, request
-from Application.Services import Services
-from Business.AccessQuestions import AccessQuestions
+from flask import Flask, render_template, flash, request, session, redirect
+from application.Services import Services
+from business.AccessQuestions import AccessQuestions
+
 
 # EB looks for an "application" callable by default
 application = Flask(__name__)
+
 application.config.from_object("config")
 Services.create_data_access("application")
 access_questions = AccessQuestions()
 
+MAX_QUESTIONS = 3
+
 @application.route("/", methods=["GET", "POST"])
 def home_page():
-    """ The homepage """
-    question_doc = access_questions.get_question()
-    answer = question_doc["answer"]
+    """The homepage"""
+    session.clear()
+    session["questions"] = access_questions.get_random_questions(MAX_QUESTIONS)
+    session["question_count"] = 0
+    return render_template("homePage.html")
+
+
+@application.route("/question_page", methods=["GET", "POST"])
+def question_page():
+    """The questions page"""
+    q_count = session["question_count"]
+
+    if q_count >= MAX_QUESTIONS:
+        return redirect("/")
+
+    question_doc = session["questions"][q_count]
+    session["question_count"] = q_count + 1
+
+    question = question_doc["question"]
+    options = question_doc["options"]
+    answer = options[question_doc["answer"]]
 
     if request.method == "POST":
         print request.form["btn"]
 
-    return render_template("homePage.html",\
-       question=question_doc["question"],\
-       options=question_doc["options"],\
-       answer=question_doc["options"][answer])
+    return render_template("questionPage.html",\
+       question=question,\
+       options=options,\
+       answer=answer)
 
 if __name__ == "__main__":
     application.run()
