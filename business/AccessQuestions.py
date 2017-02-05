57c822098611b0bd5cdc2cf29259dd0fc6eac049
samuel-peers <samuelpeers@gmail.com>
2017-02-04 20:29:51 -0600

    Implemented get random questions function

--- business/AccessQuestions.py
+++ business/AccessQuestions.py
@@ -1,20 +1,36 @@
 """
 AccessQuestions.py
 """
-from Application.Services import Services
+from json import dumps, loads
+from bson import json_util
+from application.Services import Services
 
 class AccessQuestions(object):
     """For accessing questions using the database defined in Services"""
     def __init__(self):
         self.data_access = Services.get_data_access()
 
-    def get_question(self):
-        """Grab a question from the DB"""
-        return self.data_access.get_question()
+    def get_random_questions(self, set_size):
+        """Grab a random subset of questions from the DB"""
+        num_questions = self.get_num_questions()
+        set_size = set_size if num_questions >= set_size else num_questions
 
-    def get_questions(self):
-        """get_questions"""
-        return self.data_access.get_questions()
+        questions = []
+        seen = set()
+
+        while len(seen) < set_size:
+            question = self.data_access.get_question()
+            _id = question["_id"]
+
+            if _id not in seen:
+                seen.add(_id)
+                questions.append(self.data_access.get_question())
+
+        return [loads(dumps(doc, default=json_util.default)) for doc in questions]
+
+    def get_all_questions(self):
+        """Grab all questions from the DB"""
+        return self.data_access.get_all_questions()
 
     def get_num_questions(self):
         """Return the number of questions"""
