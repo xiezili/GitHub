6e71c5c7411ba89c4b3efe97ec10a53b77eec330
samuel-peers <samuelpeers@gmail.com>
2017-02-10 10:43:52 -0600

    Implemented score keeping feature

--- application.py
+++ application.py
@@ -8,25 +8,35 @@ from business.AccessQuestions import AccessQuestions
 # EB looks for an "application" callable by default
 application = Flask(__name__)
 
-
 application.config.from_object("config")
 Services.create_data_access("application")
 access_questions = AccessQuestions()
 
 MAX_QUESTIONS = 3
+RIGHT = "right"
 
-@application.route("/", methods=["GET", "POST"])
+@application.route("/")
 def home_page():
     """The homepage"""
+
+    score = session["score"] if "score" in session.keys() else None
+
     session.clear()
     session["questions"] = access_questions.get_random_questions(MAX_QUESTIONS)
     session["question_count"] = 0
-    return render_template("homePage.html")
+    session["score"] = 0
 
+    if session["questions"] == []:
+        raise Exception("No questions found!")
+
+    return render_template("homePage.html", score=score)
 
 @application.route("/question_page", methods=["GET", "POST"])
 def question_page():
     """The questions page"""
+
+    if request.method == "POST":
+        session["score"] += 1
 
     q_count = session["question_count"]
 
@@ -37,8 +47,8 @@ def question_page():
     session["question_count"] = q_count + 1
 
     question = question_doc["question"]
-    options = question_doc["options"]
-    answer = options[question_doc["answer"]]
+    options = [str(o) for o in question_doc["options"]]
+    answer = str(options[question_doc["answer"]])
 
     return render_template("questionPage.html",\
        question=question,\
