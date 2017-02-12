efababb81afc7162934f62e09d3b7ab9e022f2ea
samuel-peers <samuelpeers@gmail.com>
2017-02-11 18:27:37 -0600

    Moved the views into their own module

--- application.py
+++ application.py
@@ -1,61 +1,25 @@
 """
 application.py
 """
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask
 from application.Services import Services
 from business.AccessQuestions import AccessQuestions
 from flask.ext.session import Session
+
+DB_NAME = "application"
 
 # EB looks for an "application" callable by default
 application = Flask(__name__)
 application.config.from_object("config")
 Session(application)
-Services.create_data_access("application")
-access_questions = AccessQuestions()
+Services.create_data_access(DB_NAME)
 
-MAX_QUESTIONS = 3
-RIGHT = "right"
+from views import home_page, question_page
 
-@application.route("/")
-def home_page():
-    """The homepage"""
-
-    score = session["score"] if "score" in session.keys() else None
-
-    session.clear()
-    session["questions"] = access_questions.get_random_questions(MAX_QUESTIONS)
-    session["question_count"] = 0
-    session["score"] = 0
-
-    if session["questions"] == []:
-        raise Exception("No questions found!")
-
-    return render_template("homePage.html", score=score)
-
-@application.route("/question_page", methods=["GET", "POST"])
-def question_page():
-    """The questions page"""
-
-    if request.method == "POST":
-        session["score"] += 1
-
-    q_count = session["question_count"]
-
-    if q_count >= MAX_QUESTIONS:
-        return redirect("/")
-
-    question_obj = session["questions"][q_count]
-
-    session["question_count"] = q_count + 1
-
-    question = question_obj.question
-    options = question_obj.options
-    answer = question_obj.answer
-
-    return render_template("questionPage.html",\
-       question=question,\
-       options=options,\
-       answer=answer)
+application.add_url_rule("/", view_func=home_page)
+application.add_url_rule\
+("/question_page", methods=["GET", "POST"], view_func=question_page)
 
 if __name__ == "__main__":
     application.run()
+    Services.close_data_access()
