2659d6a8b5da44748e977163587ce41b789c6e8a
samuel-peers <samuelpeers@gmail.com>
2017-02-11 18:29:22 -0600

    Just changed how answer is stored

--- objects/Question.py
+++ objects/Question.py
@@ -5,7 +5,8 @@ Question.py
 class Question(object):
     """For holding a question, it's options, and answer.
        The question will serve as an id"""
+
     def __init__(self, question, options, answer):
         self.question = question
         self.options = options
-        self.answer = options[answer]
+        self.answer = answer
