2e6af220d51da23eec957f50085a02d2492003a9
samuel-peers <samuelpeers@gmail.com>
2017-02-16 19:40:10 -0600

    Moved static folder to web_app dir

--- application.py
+++ application.py
@@ -3,13 +3,15 @@ application.py
 
 Note that EB looks for application.py in the root dir
 """
-
 from flask import Flask
-from web_app.application.Services import Services
 from web_app import create_app, set_up, tear_down
 
 set_up()
-application = Flask(__name__, template_folder="web_app/templates")
+
+application = Flask(__name__,\
+    template_folder="web_app/templates",\
+    static_folder="web_app/static")
+
 create_app(application)
 
 if __name__ == "__main__":
