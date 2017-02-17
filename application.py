f571ff626965cfc3f3b04b8acb0bbbc371a15c31
samuel-peers <samuelpeers@gmail.com>
2017-02-16 19:05:48 -0600

    Created a setup and teardown function

--- application.py
+++ application.py
@@ -3,18 +3,14 @@ application.py
 
 Note that EB looks for application.py in the root dir
 """
-
 from flask import Flask
-from web_app.application.Services import Services
-from web_app import create_app, DB_NAME
-
-Services.create_data_access(DB_NAME)
+from web_app import create_app, set_up, tear_down
 
 # EB looks for an "application" callable by default
 application = Flask(__name__, template_folder="web_app/templates")
-
 create_app(application)
 
 if __name__ == "__main__":
+    set_up()
     application.run()
-    Services.close_data_access()
+    tear_down()
