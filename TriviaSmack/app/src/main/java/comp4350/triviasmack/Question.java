011024a477b9b9eca41b27a50bb950853012ffa7
Emilio <krahne3@myumanitoba.ca>
2017-02-17 22:03:31 -0600

    beginning android code

---
+++ TriviaSmack/app/src/main/java/comp4350/triviasmack/Question.java
@@ -0,0 +1,19 @@
+package comp4350.triviasmack;
+
+/**
+ * Created by Emilioo on 2/17/2017.
+ */
+
+public class Question {
+    String answer;
+    String question;
+    String[] options;
+
+    public Question(String answer, String question, String[] options){
+        this.answer = answer;
+        this.question = question;
+        this.options = options;
+    }
+
+
+}
