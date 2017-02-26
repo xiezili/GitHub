8f3dc90ae312c7435f1b3e9d232a38766d5c6884
samuel-peers <samuelpeers@gmail.com>
2017-02-26 17:15:01 -0600

    Added some more unit tests

--- android_app/app/src/test/java/comp4350/triviasmack/tests/business/GameControllerTest.java
+++ android_app/app/src/test/java/comp4350/triviasmack/tests/business/GameControllerTest.java
@@ -2,9 +2,13 @@ package comp4350.triviasmack.tests.business;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
+import comp4350.triviasmack.application.Main;
 import comp4350.triviasmack.application.Services;
 import comp4350.triviasmack.business.GameController;
 import comp4350.triviasmack.business.ServerAccessStub;
+import comp4350.triviasmack.objects.Question;
 
 public class GameControllerTest extends TestCase {
 
@@ -17,7 +21,7 @@ public class GameControllerTest extends TestCase {
     @Override
     protected void setUp() throws Exception {
         Services.closeServerAccess();
-        Services.createServerAccess(new ServerAccessStub());
+        Services.createServerAccess(new ServerAccessStub(Main.numQuestions));
         dummyGameController = GameController.getInstance();
     }
 
@@ -28,29 +32,50 @@ public class GameControllerTest extends TestCase {
     }
 
     public void testSingleton(){
-        System.out.println("\nTesting GameController: Singleton");
+        System.out.println("Testing GameController: Singleton");
         GameController first = GameController.getInstance();
         GameController second = GameController.getInstance();
         assertEquals(first, second);
     }
 
     public void testStart(){
-        System.out.println("\nTesting GameController: Start");
+        System.out.println("Testing GameController: Start");
         assertFalse(dummyGameController.isStarted());
         dummyGameController.start();
         assertTrue(dummyGameController.isStarted());
     }
 
     public void testIncreaseScore(){
-        System.out.println("\nTesting GameController: IncreaseScore");
+        System.out.println("Testing GameController: IncreaseScore");
         dummyGameController.start();
         assertEquals(0, dummyGameController.getScore());
         dummyGameController.increaseScore();
         assertEquals(1, dummyGameController.getScore());
     }
 
-    public void testGetQuestion(){
-        System.out.println("\nTesting GameController: GetQuestion");
+    public void testAccessors(){
+        System.out.println("Testing GameController: Accessors");
+
         dummyGameController.start();
+
+        ArrayList<Question> questions = new ArrayList<>();
+
+        dummyGameController.getQuestions(questions);
+
+        assertNotNull(questions);
+
+        for (int i = 0; i < questions.size(); i++){
+            assertTrue(questions.get(i) instanceof Question);
+        }
+    }
+
+    public void testGetNextQuestions(){
+        System.out.println("Testing GameController: getNextQuestion");
+
+        dummyGameController.start();
+
+        Question questionObj = dummyGameController.getNextQuestion();
+
+        assertNotNull(questionObj);
     }
 }
