40e3174389dd8d286cf1d3b804fc773934230eae
Thomas Beckett <Thomas Beckett>
2017-02-26 20:16:35 -0600

    Updated unit tests for parseJSONObject

--- android_app/app/src/test/java/comp4350/triviasmack/tests/business/ParseJSONObjectTest.java
+++ android_app/app/src/test/java/comp4350/triviasmack/tests/business/ParseJSONObjectTest.java
@@ -25,29 +25,17 @@ public class ParseJSONObjectTest extends TestCase {
 
     public void testParseJSON(){
 
-        System.out.println("Testing ParseQuestion: Parse Basic JSON Object");
+        System.out.println("Testing ParseJSONObject: Parse Basic JSON Object");
         ArrayList<Question> q;
 
         try {
             JSONObject json = new JSONObject();
-            JSONObject newJSON = new JSONObject();
-
             JSONArray result = new JSONArray();
-            JSONArray options = new JSONArray();
 
-            options.put("South America");
-            options.put("Europe");
-            options.put("Australia");
-            options.put("Asia");
+            result.put(buildJSONObject());
+            json.put("result", result);
 
-            json.put("answer", new Integer(1));
-            json.put("options", options);
-            json.put("question", "The Balkans are in:");
-
-            result.put(json);
-            newJSON.put("result", result);
-
-            q = serverAccessObject.parseJSONObject(newJSON);
+            q = serverAccessObject.parseJSONObject(json);
 
             assertNotNull(q);
             assertEquals(1, q.size());
@@ -68,7 +56,7 @@ public class ParseJSONObjectTest extends TestCase {
 
     public void testBadFormat(){
 
-        System.out.println("Testing ServerAccessObject: Bad Formated JSON");
+        System.out.println("Testing ParseJSONObject: Bad Formatted JSON");
         ArrayList<Question> q;
         JSONObject json = new JSONObject();
 
@@ -83,5 +71,61 @@ public class ParseJSONObjectTest extends TestCase {
         }
     }
 
+    public void testLargeJSONArray(){
+        System.out.println("Testing ParseJSONObject: Parse Large JSON Object");
+        ArrayList<Question> q;
+        final int MAX_ARRAY_SIZE = 100;
 
+        try {
+            JSONObject json = new JSONObject();
+            JSONArray result = new JSONArray();
+
+            for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+                result.put(buildJSONObject());
+            json.put("result", result);
+
+            q = serverAccessObject.parseJSONObject(json);
+
+            assertNotNull(q);
+            assertEquals(MAX_ARRAY_SIZE, q.size());
+
+            for(int i = 0; i < MAX_ARRAY_SIZE; i++) {
+                assertEquals("The Balkans are in:", q.get(i).getQuestion());
+
+                assertEquals("South America", q.get(i).getOptions()[0]);
+                assertEquals("Europe", q.get(i).getOptions()[1]);
+                assertEquals("Australia", q.get(i).getOptions()[2]);
+                assertEquals("Asia", q.get(i).getOptions()[3]);
+
+                assertEquals(1, q.get(i).getAnswer());
+            }
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    private JSONObject buildJSONObject(){
+
+        JSONObject json;
+
+        try {
+            json = new JSONObject();
+            JSONArray options = new JSONArray();
+
+            options.put("South America");
+            options.put("Europe");
+            options.put("Australia");
+            options.put("Asia");
+
+            json.put("answer", new Integer(1));
+            json.put("options", options);
+            json.put("question", "The Balkans are in:");
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+            json = null;
+        }
+        return json;
+    }
 }
